2882b5f1f5c5e27e759021e444c89be2e91c10ca
Bruno Purper Farias <brunoplusultra@outlook.com>
2023-03-26 22:27:24 -0700

    a

--- src/server.js
+++ src/server.js
@@ -2,32 +2,50 @@ var fs = require("fs");
 var lines = fs.readFileSync("./DNIT-Distancias.csv").toString().split('\r\n');
 var citynames = lines.shift().split(';');
 var cities = new Map();
+var prompt = require('prompt-sync')();
+
 // console.log(citynames);
 
 lines.forEach((line, index) => {
     var distances = line.split(';');
     var curr = new Map();
     citynames.forEach((city, pos) => {
-         console.log(city);
+        //  console.log(city);
         curr.set(city, Number(distances[pos]));
     });
     cities.set(citynames[index], curr);
 })
 
- console.log(cities);
+//  console.log(cities.get('ARACAJU'));
 
 var allCities = citynames;
-console.log(allCities)
+console.log('Cidades Disponíveis: ',allCities)
+var control = false;
 
 function citieConfirmation(text, array) {
   if (array.includes(text)) {
-    console.log('O texto ' + text + ' existe no array.');
+    control = true;
   } else {
-    console.log('O texto ' + text + ' não existe no array.');
+    control = false;
+    console.log('A cidade ' + text + ' não existe no nosso sistema, tente novamente: ');
   }
 }
 
-//console.log(citieConfirmation('Porto Alegre', allCities))
+
+
+while (control != true) {
+  var firstCity  =  prompt('Digite a cidade de partida: ');
+  citieConfirmation(firstCity.toUpperCase(), allCities)
+  var secondCity = prompt('Digite a cidade de destino: ');
+  citieConfirmation(secondCity.toUpperCase(), allCities)
+}
+
+
+
+
+
+
+
 
 
   // ARACAJU
